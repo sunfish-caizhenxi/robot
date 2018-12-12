7060948923f2f53ca9ca0f720dbe57d1660d09da
caizx <1569875461@qq.com>
2018-12-13 00:06:09 +0800

    caizx

--- robot-service/src/main/java/cn/molasoftware/service/robot/student/StudentService.java
+++
@@ -1,8 +0,0 @@
-package cn.molasoftware.service.robot.student;
-
-import cn.molasoftware.dao.robot.pojo.Student;
-import cn.molasoftware.service.robot.base.BaseService;
-
-public interface StudentService extends BaseService<Student> {
-
-}
--- robot-wechat/src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-<span>I am ok!</span>
-</body>
-</html>
